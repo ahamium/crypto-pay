2eef367389c900fa014b644fecc5ac66fb0022ff
ahamium <ahamium@naver.com>
2026-06-24 19:02:04 -0500

    update contract address and stabilization

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 set -e
 
-echo "Running Prisma migrations..."
+# echo "Running Prisma migrations..."
 
-# 1) 정식 환경이면 보통 migrate deploy 를 사용
-#    만약 migration 파일이 없거나 실패하면, fallback 으로 db push 시도
-npx prisma migrate deploy || npx prisma db push
+# # 1) 정식 환경이면 보통 migrate deploy 를 사용
+# #    만약 migration 파일이 없거나 실패하면, fallback 으로 db push 시도
+# npx prisma migrate deploy || npx prisma db push
+
+echo "Checking writable SQLite directory..."
+mkdir -p /home/data
+touch /home/data/.write-test
+rm /home/data/.write-test
+
+echo "Running Prisma migration or db push..."
+./node_modules/.bin/prisma migrate deploy || ./node_modules/.bin/prisma db push
 
 echo "Starting NestJS app..."
 node dist/src/main.js
